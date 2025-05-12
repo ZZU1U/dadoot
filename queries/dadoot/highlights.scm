676e33c634dc6c5639bca3b669fd981560852360
Gleb Anohin <dudu@dadoot.local>
2025-05-13 01:14:47 +0300

    Reversed last change

---
+++ queries/dadoot/highlights.scm
@@ -0,0 +1,25 @@
+;; Projects
+(project
+  (line_content) @function)
+
+;; Top-level tasks
+(task
+  (priority) @constant
+  ; (description) @string
+  )
+
+;; Subtasks
+(subtask
+  (tab_indent) @punctuation.special
+  ; (subtask_description) @string
+  )
+
+;; Properties (shared between task/subtask)
+(property
+  (property_name) @attribute
+  (property_value) @number
+  )
+
+;; Optional: blank lines
+(blank_line) @comment
+
