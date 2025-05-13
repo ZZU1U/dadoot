bfe6298b55724889786e58ef26baeb0b88aa4550
Gleb Anohin <dudu@dadoot.local>
2025-05-13 09:16:30 +0300

    example func

--- lua/dadoot/module.lua
+++ lua/dadoot/module.lua
@@ -2,8 +2,22 @@
 local M = {}
 
 ---@return string
-M.my_first_function = function(greeting)
-  return greeting
+M.my_first_function = function()
+	local parser = vim.treesitter.get_parser(0, "dadoot")
+	local tree = parser:parse()[1]
+	local root = tree:root()
+
+	local ts_utils = require("nvim-treesitter.ts_utils")
+
+	local function print_nodes(node)
+		for child in node:iter_children() do
+			local text = vim.treesitter.get_node_text(child, 0)
+			print(child:type(), text)
+			print_nodes(child) -- recursive
+		end
+	end
+
+	print_nodes(root)
 end
 
 return M
